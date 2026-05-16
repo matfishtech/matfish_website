889099fc3ad02401bc354ff168d6b0f7d3dbf41c
oskaripessinen <oskaripessinen@users.noreply.github.com>
2026-05-16 19:37:50 +0000

    deploy: fa0e7ba688159e4009c62cad496ee557f08b8313

---
+++ _next/static/6WLeuY9Ryc2NMrNWDvbKX/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fabout","\u002F[lang]\u002Fblog","\u002F[lang]\u002Fblog\u002F[slug]","\u002F[lang]\u002Fcode-of-conduct","\u002F[lang]\u002Fcontact","\u002F[lang]\u002Fcookies","\u002F[lang]\u002Ffaq","\u002F[lang]\u002Fprivacy","\u002F[lang]\u002Fproducts","\u002F[lang]\u002Fterms"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
